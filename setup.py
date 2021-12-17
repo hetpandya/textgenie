dc398327489d9d5f3ee8313afe190ff714173b90
Het Pandya <hetpandya6797@gmail.com>
2021-12-17 11:50:25 +0530

    Update to v0.1.9.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="textgenie",
-    version="0.1.9.4",
+    version="0.1.9.5",
     description="A python library to augment text data using NLP.",
     long_description=long_description,
     long_description_content_type="text/markdown",
